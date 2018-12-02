4c87d4ef9b113e9d5b7710d4cb120418fd1af528
nardusstricta <gabriholz@gmail.com>
2018-12-02 12:50:19 +0100

    returen to old version

--- R/sf_soil.R
+++ R/sf_soil.R
@@ -8,13 +8,26 @@
 #'
 #' @export
 
-sf_soil <- function(data, plot_width = 3){
+sf_soil <- function(data, plot_width = 3, line_sm = T, Line = F){
   
   df <- data_mod(data)
   
   df_polygon <- cord_setting(database = df, plot_width = plot_width)
   
   shape_areas <- sf_polgon(database = df, df_polygon = df_polygon)
+  
+  mat_line <- data.frame(name= unique(df$name), numberX = df$numberX, sd = df$sd)
+  
+  df_line <- line_mod(df_polygon = df_polygon, mat_line = mat_line, sm = line_sm) 
+ 
+  
+  shape_mod  <- split_polygon(Polygon = shape_areas, Line = df_line)
+  
+  if(Line == T){
+    return(df_line)
+  }else{
+    return(shape_mod)
+  }
   
   return(shape_areas)
 }
