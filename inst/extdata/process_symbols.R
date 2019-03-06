6407fc788efa798d70416869f760dd1c380b0ef9
nardusstricta <gabriholz@gmail.com>
2019-03-06 16:33:28 +0100

    remove same special geomteries

--- inst/extdata/process_symbols.R
+++ inst/extdata/process_symbols.R
@@ -5,11 +5,14 @@ process_symbols <- data.frame(
            "Humuseinwaschung",
            "mangan Konkretionen",
            "extrovertierte Redoximorphose ed", 
-           "introvertierte Redoximorphose ed"),
-  shape = c(9, 126, 35, 15, 21, 21), 
-  color = c("black", "darkblue", "black", "black", "darkred", "grey"),
-  fill = c(NA, "darkblue", "black", NA, "grey", "darkred"),
-  stroke = c(1, 2, 2, 2, 2, 2)
+           "introvertierte Redoximorphose ed", 
+           "humos", 
+           "carbonatarm", 
+           "carbonatreich"),
+  shape = c(9, 126, 35, 15, 21, 21, 104, 60, 62), 
+  color = c("black", "darkblue", "black", "black", "darkred", "grey", "grey", "grey", "grey"),
+  fill = c(NA, "darkblue", "black", NA, "grey", "darkred", "black", "black", "black"),
+  stroke = c(1, 2, 2, 2, 4, 4, 2, 2, 2)
   
 )
 usethis::use_data(process_symbols, overwrite = TRUE)
